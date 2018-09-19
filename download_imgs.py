018c132c1e430a48f2ab0bfa0deccc20ca14cabc
manymuch <zjx219@gmail.com>
2018-09-19 16:29:47 -0400

    remove twitter credential

--- download_imgs.py
+++ download_imgs.py
@@ -9,12 +9,7 @@ import wget
 import warnings
 import os
 import argparse
-
-#Twitter API credentials
-consumer_key = "vvPRqPpKrGbWZ7342c5fgRxoE"
-consumer_secret = "kK3AzufjYCc2kNgHu9pnk5MgBXm1GvB2waGNFpxjh2USQgtkzY"
-access_key = "427126657-MRZStGDRQLNZU1KQhJsUKst9NqajLFkAZc6P3qWt"
-access_secret = "EpXwLK10ZggoVRDXF8NwJssGkgenkOzQmq5b5TVJ0fDeI"
+import twitter_api
 
 
 def setup_tweetAPI():
