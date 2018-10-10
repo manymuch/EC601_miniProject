46e5e6cc411ae2c7c4f67f529215a5c541e130aa
manymuch <zjx219@gmail.com>
2018-10-10 18:08:25 -0400

    model

--- mini2/cars.py
+++ mini2/cars.py
@@ -7,7 +7,7 @@ np.random.seed(1337)  # for reproducibility
 
 import keras.backend as K
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, BatchNormalization, MaxPooling2D,Input
+from keras.layers import Dense, Dropout, Activation, BatchNormalization, MaxPooling2D,Input,Model
 from keras.layers import Flatten
 from keras.optimizers import SGD, Adam, RMSprop
 from keras.callbacks import LearningRateScheduler
