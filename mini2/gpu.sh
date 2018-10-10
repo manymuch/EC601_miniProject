d834650d522e547e3410f6ae9e16017b20e4b98d
manymuch <zjx219@gmail.com>
2018-10-10 16:02:49 -0400

    add python

--- mini2/gpu.sh
+++ mini2/gpu.sh
@@ -8,7 +8,7 @@
 # Request 1 GPU (the number of GPUs needed should be divided by the number of CPUs requested above)
 #$ -l gpus=0.25
 
-# Specify the minimum GPU compute capability 
+# Specify the minimum GPU compute capability
 #$ -l gpu_c=3.5
 
 
@@ -16,3 +16,4 @@ module load python/2.7.13
 module load cuda/8.0
 module load cudnn/5.1
 nvidia-smi
+python cars.py
