ca87fc962d18c1dfa7b9c3dec06abe448c103ca0
manymuch <zjx219@gmail.com>
2018-10-10 14:53:38 -0400

    move mini project1 to a folder

---
+++ mini1/twitter_api.py
@@ -0,0 +1,5 @@
+#Twitter API credentials
+consumer_key = "vvPRqPpKrGbWZ7342c5fgRxoE"
+consumer_secret = "kK3AzufjYCc2kNgHu9pnk5MgBXm1GvB2waGNFpxjh2USQgtkzY"
+access_key = "427126657-MRZStGDRQLNZU1KQhJsUKst9NqajLFkAZc6P3qWt"
+access_secret = "EpXwLK10ZggoVRDXF8NwJssGkgenkOzQmq5b5TVJ0fDeI"
