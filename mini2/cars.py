95bb4cc29a29e213254a53561731cec89847d869
manymuch <zjx219@gmail.com>
2018-10-10 22:13:26 -0400

    6

--- mini2/cars.py
+++ mini2/cars.py
@@ -6,8 +6,8 @@ import cPickle
 np.random.seed(1337)  # for reproducibility
 
 import keras.backend as K
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Activation, BatchNormalization, MaxPooling2D,Input
+from keras.models import Sequential
+from keras.layers import Conv2D, Dense, Dropout, Activation, BatchNormalization, MaxPooling2D
 
 from keras.layers import Flatten
 from keras.optimizers import SGD, Adam, RMSprop
