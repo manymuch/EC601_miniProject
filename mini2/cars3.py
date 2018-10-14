f1365db5ed4f7b12c925a31ff71860625b48dc97
manymuch <zjx219@gmail.com>
2018-10-14 10:57:51 -0400

    test

--- mini2/cars3.py
+++ mini2/cars3.py
@@ -60,12 +60,10 @@ def load_cars_test():
 def preprocess(X,Y):
     X = np.reshape(X,(-1,32,32,3))/255.0
     Y = to_categorical(Y, num_classes=10)
+    return X, Y
 X_train, Y_train = load_cars_train()
-print(X_train.shape)
-print(Y_train.shape)
-
 X_train, Y_train = preprocess(X_train, Y_train)
-exit()
+
 
 
 
